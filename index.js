1430bd84ca269a1a9bdb5de14e81737b31c297c5
Hussein Talal <hossientalal2@gmail.com>
2020-05-16 09:47:03 +0000

    Done.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dodger.style.bottom = "0px";
 function moveDodgerRight(){
     let leftNumbers = dodger.style.left.replace("px","");
     let left = parseInt(leftNumbers, 10);
-    if (left<game.offsetWidth - dodger.offsetWidth) {
+    if (left<360) {
       dodger.style.left =`${left+1}px`;
     }
 }
