4afd3f87b7b284fd470a9a7114b3b8a81bbf25f8
zeena kareem <zena34269@gmail.com>
2020-05-28 13:00:31 +0000

    Done.

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ document.addEventListener("keydown", function(e) {
 function moveDodgerRight() {
  let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
- 
-  if (left > 0) {
+ console.log(leftNumbers,left)
+  if (left < 360) {
     dodger.style.left = `${left + 1}px`;
   }
 }
